409a2107596b218582487c729cca54679a8e4f51
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-06-29 17:11:23 +0100

    Adding functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,50 +1,103 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import '../App.css';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+      next: '',
+      operation: '',
+    };
+  }
+
+  eventHandler = (e) => {
+    const val = e.target.textContent;
+    this.setState((obj) => calculate(obj, val));
+  };
+
   render() {
+    const { total, next, operation } = this.state;
+    const results = `
+    ${
+  total || operation || next
+    ? `${total || ''} ${operation || ''} ${next || ''}`
+    : 0
+}`;
     return (
       <div className="calculator">
         <div className="output__section">
-          <input type="text" className="output" placeholder="0" readOnly />
+          <div className="current">{results}</div>
         </div>
+
         <div className="input__section">
           <ul className="output-ul">
-            <li id="clear" className="key">
+            <li
+              onClick={this.eventHandler}
+              onKeyDown={this.eventHandler}
+              id="clear"
+              className="key"
+            >
               AC
             </li>
-            <li id="plus-minus" className="key">
-              &#177;
+            <li onClick={this.eventHandler} id="plus-minus" className="key">
+              +/-
             </li>
-            <li id="percent" className="key">
-              &#37;
+            <li onClick={this.eventHandler} id="percent" className="key">
+              %
             </li>
-            <li id="divide" className="key action">
-              &#247;
+            <li onClick={this.eventHandler} id="divide" className="key action">
+              ÷
             </li>
-            <li className="key">7</li>
-            <li className="key">8</li>
-            <li className="key">9</li>
-            <li id="times" className="key action">
-              &#215;
+            <li onClick={this.eventHandler} className="key">
+              7
             </li>
-            <li className="key">4</li>
-            <li className="key">5</li>
-            <li className="key">6</li>
-            <li id="minus" className="key action">
-              &#8722;
+            <li onClick={this.eventHandler} className="key">
+              8
             </li>
-            <li className="key">1</li>
-            <li className="key">2</li>
-            <li className="key">3</li>
-            <li id="plus" className="key action">
-              &#x2b;
+            <li onClick={this.eventHandler} className="key">
+              9
             </li>
-            <li className="key zero">0</li>
-            <li className="key decimal">&#8901;</li>
-            <li id="equals" className="key action">
-              &#61;
+            <li onClick={this.eventHandler} id="times" className="key action">
+              x
+            </li>
+            <li onClick={this.eventHandler} className="key">
+              4
+            </li>
+            <li onClick={this.eventHandler} className="key">
+              5
+            </li>
+            <li onClick={this.eventHandler} className="key">
+              6
+            </li>
+            <li onClick={this.eventHandler} id="minus" className="key action">
+              -
+            </li>
+            <li onClick={this.eventHandler} className="key">
+              1
+            </li>
+            <li onClick={this.eventHandler} className="key">
+              2
+            </li>
+            <li onClick={this.eventHandler} className="key">
+              3
+            </li>
+            <li onClick={this.eventHandler} id="plus" className="key action">
+              +
+            </li>
+            <li onClick={this.eventHandler} className="key zero">
+              0
+            </li>
+            <li onClick={this.eventHandler} className="key decimal">
+              .
+            </li>
+            <li onClick={this.eventHandler} id="equals" className="key action">
+              =
             </li>
           </ul>
         </div>
